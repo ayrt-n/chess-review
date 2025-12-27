71cfd168299e271dbd3693289169908af99a3cdf
ayrton <ayrton.parkinson1@gmail.com>
2025-12-27 12:54:48 -0500

    Add getters and setters

--- backend/src/main/java/com/chess/api/model/MoveAnalysis.java
+++ backend/src/main/java/com/chess/api/model/MoveAnalysis.java
@@ -8,23 +8,80 @@ public class MoveAnalysis implements Serializable {
   private String uci;
 
   private String best_san;
-  private List<String> pv_san;
-  private int eval_cp;
-  private int eval_cp_delta;
+  private List<String> pvSan;
+  private int evalCp;
+  private int evalCpDelta;
 
   private String commentary;
   private MoveClassification classification;
 
   public MoveAnalysis() {}
 
-  public MoveAnalysis(String san, String uci, String best_san, List<String> pv_san, int eval_cp, int eval_cp_delta, String commentary, MoveClassification classication) {
+  public MoveAnalysis(String san) {
     this.san = san;
+  }
+
+  public String getSan() {
+    return san;
+  }
+
+  public void setSan(String san) {
+    this.san = san;
+  }
+
+  public String getUci() {
+    return uci;
+  }
+
+  public void setUci(String uci) {
     this.uci = uci;
+  }
+
+  public String getBestSan() {
+    return best_san;
+  }
+
+  public void setBestSan(String best_san) {
     this.best_san = best_san;
-    this.pv_san = pv_san;
-    this.eval_cp = eval_cp;
-    this.eval_cp_delta = eval_cp_delta;
+  }
+
+  public List<String> getPvSan() {
+    return pvSan;
+  }
+
+  public void setPvSan(List<String> pv_san) {
+    this.pvSan = pv_san;
+  }
+
+  public int getEvalCp() {
+    return evalCp;
+  }
+
+  public void setEvalCp(int eval_cp) {
+    this.evalCp = eval_cp;
+  }
+
+  public int getEvalCpDelta() {
+    return evalCpDelta;
+  }
+
+  public void setEvalCpDelta(int eval_cp_delta) {
+    this.evalCpDelta = eval_cp_delta;
+  }
+
+  public String getCommentary() {
+    return commentary;
+  }
+
+  public void setCommentary(String commentary) {
     this.commentary = commentary;
-    this.classification = classication;
+  }
+
+  public MoveClassification getClassification() {
+    return classification;
+  }
+
+  public void setClassification(MoveClassification classification) {
+    this.classification = classification;
   }
 }
